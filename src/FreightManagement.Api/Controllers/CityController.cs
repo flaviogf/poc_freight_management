a0de32c4bef46478163b241a70329f599d4d4bff
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:07:13 -0300

    feat: adding City controller

---
+++ src/FreightManagement.Api/Controllers/CityController.cs
@@ -0,0 +1,26 @@
+﻿using FreightManagement.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreightManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("State/{stateId}/[controller]")]
+    public class CityController : ControllerBase
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public CityController(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index([FromRoute] int stateId)
+        {
+            var cities = await _cityRepository.FindAll(stateId);
+
+            return Ok(cities);
+        }
+    }
+}
