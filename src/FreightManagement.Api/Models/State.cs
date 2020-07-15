70cf55efd09f5bce96e76f0af23c6d16a390fc43
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:04:37 -0300

    feat: adding state class

---
+++ src/FreightManagement.Api/Models/State.cs
@@ -0,0 +1,13 @@
+﻿namespace FreightManagement.Api.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string BeginZipCode { get; set; }
+
+        public string EndZipCode { get; set; }
+    }
+}
