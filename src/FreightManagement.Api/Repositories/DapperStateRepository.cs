5b3eb2d2fec69f0283ec729a00d4375c6c2276bc
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:05:38 -0300

    feat: adding State repository

---
+++ src/FreightManagement.Api/Repositories/DapperStateRepository.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using FreightManagement.Api.Infrastructure;
+using FreightManagement.Api.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace FreightManagement.Api.Repositories
+{
+    public class DapperStateRepository : IStateRepository
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly ILogger<DapperStateRepository> _logger;
+
+        public DapperStateRepository(IUnitOfWork uow, ILogger<DapperStateRepository> logger)
+        {
+            _uow = uow;
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<State>> FindAll()
+        {
+            try
+            {
+                var procedure = "usp_Estado_Sel";
+
+                var cities = await _uow.Connection.QueryAsync<State>(procedure, transaction: _uow.Transaction, commandType: CommandType.StoredProcedure);
+
+                return cities;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return new List<State>();
+            }
+        }
+    }
+}
---
+++ src/FreightManagement.Api/Repositories/IStateRepository.cs
@@ -0,0 +1,11 @@
+﻿using FreightManagement.Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreightManagement.Api.Repositories
+{
+    public interface IStateRepository
+    {
+        Task<IEnumerable<State>> FindAll();
+    }
+}
