96f79f63f3946b2f12f6a37625bc531d1e2fec24
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:04:50 -0300

    feat: adding City class

---
+++ src/FreightManagement.Api/Models/City.cs
@@ -0,0 +1,13 @@
+﻿namespace FreightManagement.Api.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string BeginZipCode { get; set; }
+
+        public string EndZipCode { get; set; }
+    }
+}
