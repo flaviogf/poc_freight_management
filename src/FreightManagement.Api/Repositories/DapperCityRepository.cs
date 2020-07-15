10bc56c169186b1ffffbdabb5d80a5b5b4bccff7
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:06:36 -0300

    feat: adding City repository

---
+++ src/FreightManagement.Api/Repositories/DapperCityRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using FreightManagement.Api.Infrastructure;
+using FreightManagement.Api.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace FreightManagement.Api.Repositories
+{
+    public class DapperCityRepository : ICityRepository
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly ILogger<DapperCityRepository> _logger;
+
+        public DapperCityRepository(IUnitOfWork uow, ILogger<DapperCityRepository> logger)
+        {
+            _uow = uow;
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<City>> FindAll(int stateId)
+        {
+            try
+            {
+                var procedure = "usp_Cidade_Sel_IdEstado";
+
+                var param = new
+                {
+                    IdEstado = stateId
+                };
+
+                var cities = await _uow.Connection.QueryAsync<City>(procedure, param: param, transaction: _uow.Transaction, commandType: CommandType.StoredProcedure);
+
+                return cities;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex);
+
+                return new List<City>();
+            }
+        }
+    }
+}
---
+++ src/FreightManagement.Api/Repositories/ICityRepository.cs
@@ -0,0 +1,11 @@
+﻿using FreightManagement.Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreightManagement.Api.Repositories
+{
+    public interface ICityRepository
+    {
+        Task<IEnumerable<City>> FindAll(int stateId);
+    }
+}
