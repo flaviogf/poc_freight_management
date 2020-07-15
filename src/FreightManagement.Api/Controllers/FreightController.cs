1d911b068b8473ca4d0a36737473bc74183e1cbd
flaviogf <flavio.fernandes6@gmail.com>
2020-07-14 21:24:23 -0300

    feat: adding freight controller

---
+++ src/FreightManagement.Api/Controllers/FreightController.cs
@@ -0,0 +1,25 @@
+﻿using FreightManagement.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreightManagement.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FreightController : Controller
+    {
+        private readonly IFreightRepository _freightRepository;
+
+        public FreightController(IFreightRepository freightRepository)
+        {
+            _freightRepository = freightRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var freights = await _freightRepository.FindAll();
+
+            return Ok(freights);
+        }
+    }
+}
