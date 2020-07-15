7b08f2566a101add65c89c9b47a8f05e96f1089c
flaviogf <flavio.fernandes6@gmail.com>
2020-07-14 21:23:38 -0300

    feat: adding freight value model

---
+++ src/FreightManagement.Api/Models/FreightValue.cs
@@ -0,0 +1,21 @@
+﻿namespace FreightManagement.Api.Models
+{
+    public class FreightValue
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string BeginZipCode { get; set; }
+
+        public string EndZipCode { get; set; }
+
+        public double BeginWeight { get; set; }
+
+        public double EndWeight { get; set; }
+
+        public int Deadline { get; set; }
+    }
+}
