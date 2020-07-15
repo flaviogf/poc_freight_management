5ee097e774c41d5e1d096f27c7ca3c1c8a341068
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:04:12 -0300

    feat: adding Maybe struct

---
+++ src/FreightManagement.Api/Infrastructure/Maybe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FreightManagement.Api.Infrastructure
+{
+    public struct Maybe<T>
+    {
+        private T _value;
+
+        private Maybe(T value)
+        {
+            _value = value;
+        }
+
+        public T Value => _value ?? throw new InvalidOperationException();
+
+        public bool HasNoValue => _value == null;
+
+        public static implicit operator Maybe<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+    }
+}
