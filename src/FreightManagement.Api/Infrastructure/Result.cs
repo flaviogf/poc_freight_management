d7105a96a640a6c8e48740fb94c8f68795bc0212
flaviogf <flavio.fernandes6@gmail.com>
2020-07-15 12:04:23 -0300

    feat: adding Result class

---
+++ src/FreightManagement.Api/Infrastructure/Result.cs
@@ -0,0 +1,47 @@
+﻿namespace FreightManagement.Api.Infrastructure
+{
+    public class Result
+    {
+        protected Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+
+        public bool Failure => !Success;
+
+        public static Result Ok()
+        {
+            return new Result(true, string.Empty);
+        }
+
+        public static Result Fail(string message)
+        {
+            return new Result(false, message);
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>(value, true, string.Empty);
+        }
+
+        public static Result<T> Fail<T>(string message)
+        {
+            return new Result<T>(default, false, message);
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public Result(T value, bool success, string message) : base(success, message)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+    }
+}
