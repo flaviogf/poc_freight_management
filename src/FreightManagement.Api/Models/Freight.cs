85b0950fb07a3f6a64b94bc525cd792c09715ceb
flaviogf <flavio.fernandes6@gmail.com>
2020-07-14 21:23:24 -0300

    feat: adding freight model

---
+++ src/FreightManagement.Api/Models/Freight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FreightManagement.Api.Models
+{
+    public class Freight
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<FreightValue> Values { get; set; } = new List<FreightValue>();
+    }
+}
